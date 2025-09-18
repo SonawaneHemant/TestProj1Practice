c39054ee2a800096a8be0a01d2d27670135fa8c7
SonawaneHemant <hemant.s@orioninc.com>
2025-09-18 11:56:49 +0530

    Next Changes with DB

---
+++ API/Controllers/MembersController.cs
@@ -0,0 +1,89 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")] //https://localhost:7001/api/members
+    [ApiController]
+    public class MembersController(AppDbContext context) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<IReadOnlyList<AppUser>>> GetMembers()
+        {
+            var members = await context.AppUsers.ToListAsync();
+            return members;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetMembers(string id)
+        {
+            var member = await context.AppUsers.FindAsync(id);
+            if (member == null) return NotFound();
+            return member;
+        }
+    }
+}
+
+//         private readonly AppDbContext _context = context;
+
+//         [HttpGet]
+//         public IActionResult GetMembers()
+//         {
+//             var members = _context.Members.ToList();
+//             return Ok(members);
+//         }
+
+//         [HttpGet("{id}")]
+//         public IActionResult GetMember(int id)
+//         {
+//             var member = _context.Members.Find(id);
+//             if (member == null)
+//             {
+//                 return NotFound();
+//             }
+//             return Ok(member);
+//         }
+
+//         [HttpPost]
+//         public IActionResult CreateMember(Member member)
+//         {
+//             _context.Members.Add(member);
+//             _context.SaveChanges();
+//             return CreatedAtAction(nameof(GetMember), new { id = member.Id }, member);
+//         }
+
+//         [HttpPut("{id}")]
+//         public IActionResult UpdateMember(int id, Member updatedMember)
+//         {
+//             var member = _context.Members.Find(id);
+//             if (member == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             member.Name = updatedMember.Name;
+//             member.Email = updatedMember.Email;
+//             // Update other properties as needed
+
+//             _context.SaveChanges();
+//             return NoContent();
+//         }
+
+//         [HttpDelete("{id}")]
+//         public IActionResult DeleteMember(int id)
+//         {
+//             var member = _context.Members.Find(id);
+//             if (member == null)
+//             {
+//                 return NotFound();
+//             }
+
+//             _context.Members.Remove(member);
+//             _context.SaveChanges();
+//             return NoContent();
+//         }
+//     }
+// }
